35965b7c3ab6755e58ddd87d6b75d11c1893afe4
TIGER <141488144+tiger7815@users.noreply.github.com>
2023-12-07 04:01:31 +0530

    Update details.py

--- details.py
+++ details.py
@@ -3,5 +3,5 @@ api_hash = "3506d8474ad1f4f5e79b7c52a5c3e88d"
 bot_token = "6423225037:AAHgzQIADOPADZD1Bk2LZSEl5_gl438NiWg"
 auth_users = [6175650047,6981453498]
 sudo_user = -1002130111832
-log_channel = -1004023643109
-txt_channel = -1002070057679
+log_channel = -1002002946631
+txt_channel = -1002002946631
